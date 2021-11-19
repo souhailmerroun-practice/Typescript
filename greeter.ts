97508896926a25ae1220d9765023444598eb22e1
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-11-19 14:16:36 +0100

    Using class

--- greeter.ts
+++ greeter.ts
@@ -1,12 +1,23 @@
+class Student {
+    fullName: string;
+
+    constructor(
+        public firstName: string,
+        public lastName: string
+    ) {
+        this.fullName = firstName + " " + lastName
+    }
+}
+
 interface Person {
-    firstName: string,
-    lastName: string
+    firstName: string;
+    lastName: string;
 }
 
 function greeter(person: Person) {
     return "Hello" + person.firstName + " " + person.lastName;
 }
 
-let user = { firstName: "John", lastName: "Doe" }
+let user = new Student("John", "Doe");
 
 document.body.textContent = greeter(user);
