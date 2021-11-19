5718b874ed6800c675f3e1e3e54a25786b9ebbda
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-11-19 14:14:50 +0100

    Using interface

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,12 @@
-function greeter(person: string) {
-    return "Hello" + person;
+interface Person {
+    firstName: string,
+    lastName: string
 }
 
-let user = "Jane User";
+function greeter(person: Person) {
+    return "Hello" + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "John", lastName: "Doe" }
 
 document.body.textContent = greeter(user);
